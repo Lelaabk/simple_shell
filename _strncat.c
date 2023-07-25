8cc488f804beda988a651d9a17376966d64ef3ca
Lelaabk <lolabkari@gmail.com>
2023-07-25 11:07:57 -0700

    strncat file added

---
+++ _strncat.c
@@ -0,0 +1,24 @@
+/**
+* _strncat - concat string.
+* @dest: destination str.
+* @src: src str.
+* Return: output.
+*/
+
+char *_strncat(char *dest, const char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
