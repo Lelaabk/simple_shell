c99c4bae02c679a29bfc15f576d80bb345d7b204
Mohamed Ismael <mohamed.adel.ism@outlook.com>
2023-07-25 19:17:52 +0200

    prompt added

---
+++ printprompt.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+	* printprompt - helper function , print prompt.
+	* @username: input name.
+*/
+
+void printprompt(char *username)
+{
+	_puts(username);
+	_puts("//:$");
+}
