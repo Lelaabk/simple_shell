6bc6d8e2e3451125917ddb3423e875239c8871c4
Lelaabk <lolabkari@gmail.com>
2023-07-25 11:11:25 -0700

    final prompt file added

---
+++ final_propmt.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+	* prompt - responsible for spawn prompt, respawn after each execution.
+	* @arg: input arg.
+	* Return: exit on failure.
+*/
+
+void prompt(char *arg)
+{
+	char *buff = NULL;
+	size_t buffsize = 0;
+	pid_t ex_pid;
+	int status = 0;
+	char **cmd;
+
+	printf("arg =%s ", arg);
+	for (;;)
+	{
+		printprompt("mega-team");
+		fflush(stdin);
+		if (getline(&buff, &buffsize, stdin) == -1)
+		{
+			_free(buff);
+			exit(0);
+		}
+		cmd = removetokens(buff, "\n \t");
+
+		ex_pid = fork();
+		if (ex_pid == 0)
+		{
+			execute(cmd);
+		}
+		else
+		{
+			wait(&status);
+			_freeptr(cmd);
+			_free(buff);
+		}
+	}
+}
